2f7c35b025e044eda477b8e0412dd401b64f5e4a
Blessed Sibanda <blessedsibanda434@gmail.com>
2019-01-11 17:18:57 +0200

    blog models

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.shortcuts import reverse
+from tinymce.models import HTMLField
 
 class Post(models.Model):
     STATUS = (
@@ -7,7 +8,7 @@ class Post(models.Model):
         ('Published', 'P')
     )
     title = models.CharField(max_length=150)
-    body = models.TextField()
+    body = HTMLField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     image_header = models.ImageField(upload_to='images/image_headers', null=True, blank=True)
